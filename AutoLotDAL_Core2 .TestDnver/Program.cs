fc5eb43dbe96a5d27e7724dc0dc639dfbc881314
savemysoul382 <serjke88@gmail.com>
2020-05-04 12:43:36 +0300

    + Add AutoLotDAL_Core2 .Models Project (Core Library) + AutoLotDAL_Core2 .TestDnver Console Project (Core)

---
+++ AutoLotDAL_Core2 .TestDnver/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace AutoLotDAL_Core2_.TestDnver
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
